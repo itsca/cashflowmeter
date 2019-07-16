e68b05e7b4045138b140458c49a1b1e4c59ecf21
Itsca <itscae@gmail.com>
2019-07-15 21:00:55 -0600

    • Adds styles to calculator wrapper

--- src/Containers/Calculator/Calculator.tsx
+++ src/Containers/Calculator/Calculator.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Grid, List, ListItem, ListItemText, Paper, Button, Divider, Card } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '2%'
+  }
+});
 
 const Calculator: React.FC = () => {
+
+  const classes = useStyles();
+
   return (
-    <Grid item xs={12}>
+    <Grid item xs={12} className={classes.root}>
       <Grid item xs={12}>
         <Card>
           <Typography variant="h2">
