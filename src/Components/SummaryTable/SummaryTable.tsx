4492528f9e70a6ff8dbe2e5ee00cac0c3d875bae
Itsca <itscae@gmail.com>
2019-07-25 20:51:44 -0600

    • Adds basic failes copyed from Layout

---
+++ src/Components/SummaryTable/SummaryTable.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Grid, Paper } from '@material-ui/core';
+import HomePage from '../../Containers/HomePage/HomePage';
+import MainBar from '../MainBar/MainBar';
+import { Route, Link } from 'react-router-dom';
+import Calculator from '../../Containers/Calculator/Calculator';
+
+const SummaryTable: React.FC = () => {
+  return (
+    <Grid container spacing={2} className="mainContainer">
+      <Grid container>
+        <MainBar />
+      </Grid>
+      <Grid container>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/calculator" exact component={Calculator} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default SummaryTable;
