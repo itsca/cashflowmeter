832897c435ea59c87873cf432d73729345e746ce
Itsca <itscae@gmail.com>
2019-11-02 15:37:09 -0600

    • Ads basic logic for SummaryTableItem

--- src/Components/SummaryTable/SummaryTableBody/SummaryTableBody.tsx
+++ src/Components/SummaryTable/SummaryTableBody/SummaryTableBody.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TableBody, TableCell, TableRow, Checkbox, TextField } from '@material-ui/core';
 import { SummaryTableSortingOrder, SummaryTableRowData } from '../SummaryTable';
+import SummaryTableItem from '../SummaryTableItem/SummaryTableItem';
 
 interface State {
   values: {
@@ -73,35 +74,7 @@ const SummaryTableBody: React.FC<Props> = (props: Props) => {
           const labelId = `enhanced-table-checkbox-${index}`;
 
           return (
-            <TableRow
-              hover
-              role="checkbox"
-              aria-checked={isItemSelected}
-              tabIndex={-1}
-              key={row.name}
-              selected={isItemSelected}
-              >
-              <TableCell padding="checkbox">
-                <Checkbox
-                  checked={isItemSelected}
-                  inputProps={{ 'aria-labelledby': labelId }}
-                  onClick={event => handleSelectClick(event, rowName)}
-                />
-              </TableCell>
-              <TableCell component="th" id={labelId} scope="row" padding="none">
-                {/* <TextField
-                  id="input-name"
-                  label="Name"
-                  className={classes.textField}
-                  value={values.name}
-                  onChange={handleChange('name')}
-                  margin="normal"
-                  variant="outlined"
-                /> */}
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
-            </TableRow>
+            <SummaryTableItem row={row} labelId={labelId} rowName={rowName} isItemSelected={isItemSelected} handleSelectClick={handleSelectClick}/>
           );
         })}
       {emptyRows > 0 && (
--- src/Components/SummaryTable/SummaryTableItem/SummaryTableItem.tsx
+++ src/Components/SummaryTable/SummaryTableItem/SummaryTableItem.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, TableRow, TableCell, Checkbox } from '@material-ui/core';
+import { SummaryTableRowData } from '../SummaryTable';
 
-const SummaryTableItem: React.FC = () => {
+interface Props {
+  labelId: string,
+  row: SummaryTableRowData,
+  rowName: string,
+  isItemSelected: boolean,
+  handleSelectClick: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, rowName: string) => void
+}
+
+const SummaryTableItem: React.FC<Props> = (props: Props) => {
+  const {row, isItemSelected, handleSelectClick, labelId, rowName} = props
   return (
-    <Grid className="SummaryTableItem">
-      SummaryTableItem
-    </Grid>
+    <TableRow
+      hover
+      role="checkbox"
+      aria-checked={isItemSelected}
+      tabIndex={-1}
+      key={row.name}
+      selected={isItemSelected}
+      >
+      <TableCell padding="checkbox">
+        <Checkbox
+          checked={isItemSelected}
+          inputProps={{ 'aria-labelledby': labelId }}
+          onClick={event => handleSelectClick(event, rowName)}
+        />
+      </TableCell>
+      <TableCell component="th" id={labelId} scope="row" padding="none">
+        {/* <TextField
+          id="input-name"
+          label="Name"
+          className={classes.textField}
+          value={values.name}
+          onChange={handleChange('name')}
+          margin="normal"
+          variant="outlined"
+        /> */}
+        {row.name}
+      </TableCell>
+      <TableCell align="right">{row.amount}</TableCell>
+    </TableRow>
   );
 }
 
